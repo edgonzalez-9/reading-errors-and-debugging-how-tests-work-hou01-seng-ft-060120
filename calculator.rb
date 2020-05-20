5b2285b201a7f6616ad250c846ddcff0a7422f2f
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-20 14:52:55 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,26 @@
-# Add your variables here
+first_number = 3
+second_number = 9
+
+def sum
+  sum_r = first_number - second_number
+  puts sum_r
+  sum_r
+end
+
+def difference
+  dif_r = first_number - second_number
+  puts dif_r
+  dif_r
+end
+
+def product
+  product_r = first_number * second_number
+  puts product_r
+  product_r
+end
+
+def quotient
+  quotient_r = first_number/second_number
+  puts quotient_r
+  quotient_r
+end
